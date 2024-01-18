5a75de1c7c4f1c8255bc7bd5960c473bcd2f4418
rweil55 <github@shaw-weil.com>
2024-01-17 21:36:29 -0500

    githeb.bat

--- gitpush.bat
+++ gitpush.bat
@@ -1,5 +1,5 @@
 
 cd /inetpub/wwwroot/w/troyer
 git add .
-git commit -m "from indesign 2024-01-08 00:22" --no-edit
+git commit -m "from indesign 2024-01-11 13:14" --no-edit
 git push https://github.com/rweil55/troyer
