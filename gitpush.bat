4ce48506e11c825d573690d9c66974f31ac5a56b
rweil55 <github@shaw-weil.com>
2024-10-16 06:44:06 -0400

    from indesign 2024-10-16 10:42

--- gitpush.bat
+++ gitpush.bat
@@ -1,5 +1,13 @@
 
 cd /inetpub/wwwroot/w/troyer
+
+
 git add .
-git commit -m "from indesign 2024-01-11 13:14" --no-edit
-git push https://github.com/rweil55/troyer
+git commit -m "from indesign 2024-10-16 10:42" --no-edit
+git push https://github.com/rweil55/web-troyer main
+
+rem wait for one minute to allow git hub to get its act together
+timeout 60
+rem go fo it!!
+timeout 5
+exit
